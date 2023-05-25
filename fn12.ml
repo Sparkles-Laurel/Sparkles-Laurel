80231bc940747d55146c583a611232a4e7d92c96
Yiğit Cemal Öztürk <ozturkkivilcimcemal@outlook.com.tr>
2023-05-25 09:52:36 +0300

    Create fn12.ml

---
+++ fn12.ml
@@ -0,0 +1,14 @@
+open Math;
+
+fun f1 = sin(xr)k
+fun f2 = cos(xr)k
+fun f3 = cos((x+3)r)k
+fun f4 = sin((x+3)r)k
+fun f5 = sin((x+6)r)k
+fun f6 = cos((x+6)r)k
+fun f7 = sin((x+9)r)k
+fun f8 = cos((x+9)r)k
+fun f9 = sin((x+12)r)k
+fun f10 = cos((x+12)r)k
+fun f11 = -k
+fun f12 = +k
